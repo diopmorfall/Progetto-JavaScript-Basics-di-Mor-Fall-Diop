7662910b9b932190850e712dcaee41838adb0c05
diopmorfall <morfall.papydiop99@gmail.com>
2021-10-28 19:25:38 +0200

    Initialization of the project

---
+++ assets/js/counter.js
@@ -0,0 +1,2 @@
+//* let's handle the counter here: initially 0, then increment/decrement with buttons
+//* additional features: increment with +/- keys of the numpad
---
+++ assets/js/setup.js
@@ -0,0 +1,1 @@
+//* all elements are created here
