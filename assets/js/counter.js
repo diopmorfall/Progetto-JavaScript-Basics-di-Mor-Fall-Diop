a588f1a0e2dbf49dd8315ed5d1a81c41b545566b
diopmorfall <morfall.papydiop99@gmail.com>
2021-11-01 18:11:09 +0100

    Basic implementation of the requested funcionalities of the counter, with the addition of the reset one

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,2 +1,39 @@
-//* let's handle the counter here: initially 0, then increment/decrement with buttons
+let plus = document.querySelector(".counter").firstElementChild;
+let minus = document.querySelector(".counter").lastElementChild;
+let result = document.querySelector("span");
+
+function increaseValue(){
+    let finalValue = Number(result.textContent) + 1;
+    showResult(finalValue);
+}
+
+function decreaseValue(){
+    let finalValue = Number(result.textContent) - 1;
+    showResult(finalValue);
+}
+
+function showResult(value){
+    result.textContent = value;
+}
+
+function resetCounter(){
+    showResult(0);
+}
+
+plus.addEventListener("click", increaseValue);
+minus.addEventListener("click", decreaseValue);
+result.addEventListener("click", resetCounter);
+
+document.addEventListener("keydown", function(event){
+    switch(event.code){
+        case "NumpadAdd":
+            increaseValue();
+            break;
+
+        case "NumpadSubtract":
+            decreaseValue();
+            break;
+    }
+});
+
 //* additional features: increment with +/- keys of the numpad
